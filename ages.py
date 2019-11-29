bbf8a2cc9398a1e45bce6d681a99b24ac3f662e9
gness1804 <graham.nessler@gmail.com>
2019-11-29 16:43:53 -0600

    More for ages

--- ages.py
+++ ages.py
@@ -5,3 +5,17 @@ for age in ages:
   count += age
 
 print(str(count) + ' is the total age.')
+
+ages2 = [78, 34, 17, 32, 49]
+
+adults = []
+
+for item in ages2:
+  if item >= 18:
+    adults.append(item)
+
+print(adults)
+
+for i in range(4):
+  print(i)
+
