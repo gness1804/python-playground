1d52df6f899c38c5cf558e1f3dc229d0e9f19e3d
gness1804 <graham.nessler@gmail.com>
2019-11-30 15:06:22 -0600

    Refactor to return the players from findWinless, not just print them

--- players.py
+++ players.py
@@ -1,13 +1,16 @@
 titles = {
   'Lebron James': 3,
+  'Dak Prescott': 0,
   'Tom Brady': 6,
   'Anthony Davis': 0,
 }
 
 def findWinless(obj):
   '''Shows each player who has not yet won a title.'''
+  res = []
   for player in titles:
     if titles[player] == 0:
-      print(player + ' has not won a title yet.')
+      res.append(player)
+  return res
 
-findWinless(titles)
+print(findWinless(titles))
